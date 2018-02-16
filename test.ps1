411deb05b3cef487c0192754a2bcbe743443e11b
Marc van Eijk <marcvaneijk@msn.com>
2018-02-16 23:28:32 +0100

    updatedOutput

--- test.ps1
+++ test.ps1
@@ -1,55 +1,32 @@
-﻿## Get Token from existing powershell session
-
-$tenantId = (Get-AzureRmSubscription -SubscriptionName 'mavane').TenantId
-$context = Get-AzureRmContext
-$cache = $context.TokenCache
-$token = $cache.ReadItems() | where {$_.tenantId -eq $tenantId }
-
-## validate a template against the rest api
-try {
-
-$mainTemplateUri = "https://raw.githubusercontent.com/marcvaneijk/arm-template-functions/master/string/base64ToJson.json"
-$subscriptionId = "c18cac5d-e2c2-47d7-877a-71e17b3bfad1"
-$apiVersion = "2016-09-01"
-$deploymentName = ([guid]::NewGuid()).guid
-$resourceGroupName = "TemplateValidation"
-$url = {https://management.azure.com/subscriptions/{0}/resourcegroups/{1}/providers/Microsoft.Resources/deployments/{2}/validate?api-version={3}} -f $subscriptionId, $resourceGroupName, $deploymentName, $apiVersion
-
-        $body = @{
-            "properties"=@{
-                "mode"="Incremental"
-                "debugSetting"=@{"detailLevel"="none"} ## "detailLevel"="requestContent,responseContent"
-                "templateLink"=@{"uri"=$mainTemplateUri}
-            }
-        }
-
-        $deploymentArgs = @{
-            "ContentType" = "application/json"
-            "Headers" = @{
-            "authorization"="Bearer $($token.AccessToken)"
-            }
-            "Body" = ($Body | ConvertTo-Json)
-            "Method" = "Post"
-            "Uri" = $url
-        }
-
-        Invoke-RestMethod @deploymentArgs
-
-}
-catch{}
-
-$result.properties.provisioningState
-
-
-### Using Test-AzureRmDeployment
+﻿### Using Test-AzureRmDeployment
 
 Login-AzureRmAccount
 Get-AzureRmSubscription -SubscriptionName "mavane" | Select-AzureRmSubscription 
 
 
-$templatefunctions = Get-ChildItem -Path C:\git\arm-template-functions -File -Filter "*.json" -Recurse
-$templatefunctions[45..48] | ForEach {
-write-host ""
-write-host $_.Name -ForegroundColor Yellow
-Test-AzureRmResourceGroupDeployment -ResourceGroupName "TemplateValidation" -TemplateFile $_.FullName -Verbose
+$result = @()
+$count = 1
+$percentage = 100/$templatefunctions[0..5].count
+
+
+ForEach ($function in $templatefunctions[0..5]) {
+
+    try{
+        $DebugPreference = 'Continue'
+        $output = Test-AzureRmResourceGroupDeployment -ResourceGroupName "TemplateValidation" -TemplateFile $function.FullName 5>&1 -ErrorAction SilentlyContinue
+        $DebugPreference = 'SilentlyContinue'
+        $message = ($output | Where-Object { $_ -like "*HTTP RESPONSE*"}).Message
+        $start = $message.IndexOf('Status Code:')+14
+        $end = $message.substring($start).IndexOf('Headers:')-4
+        $result += @{($function.Directory.Name + '.' + $function.BaseName) = $message.substring($start,$end)}  
+    }
+    catch{
+        $result += @{($function.Directory.Name + '.' + $function.BaseName) = "Failed"}
+    }
+
+
+    Write-Progress -Activity "Search in Progress" -Status "($function.Directory.Name + '.' + $function.BaseName)" -PercentComplete [int]($percentage*$count)
+    $count+1
 }
+
+$result
